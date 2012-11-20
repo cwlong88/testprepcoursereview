618d3dcaaa6ab2c6a7f0d6f832e66ab87e658c01
Chris Long <cwlong88@yahoo.com>
2012-11-19 21:45:05 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testprepcoursereview::Application.config.secret_token = '44b3385973920b9684db065876cd5f9851354a81170824ab72f3fd6ff360cde42d94c12d555c62ab9efc74f01bf0665eb01527def04a3366d854b5fa94a67c68'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Testprepcoursereview::Application.config.session_store :cookie_store, key: '_testprepcoursereview_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Testprepcoursereview::Application.config.session_store :active_record_store
